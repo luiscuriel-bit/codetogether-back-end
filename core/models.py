a07586024fae7d808d55d7da8a2b3ba493a41b77
Luis Curiel De Sousa <luisfrancisco.lfcds@outlook.com>
2025-02-19 19:18:05 -0500

    Add database models

---
+++ core/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Collaborator(models.Model):
+    ROLE_CHOICES = [("admin", "Admin"), ("editor", "Editor"), ("viewer", "Viewer")]
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    role = models.CharField(max_length=6, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.role
+
+
+class Notification(models.Model):
+    STATUS_CHOICES = [("unread", "Unread"), ("read", "Read")]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    status = models.CharField(max_length=6, choices=STATUS_CHOICES, default="unread")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message

2d8c76f841de8f416e82c58c98de9f98159da826
Luis Curiel De Sousa <luisfrancisco.lfcds@outlook.com>
2025-02-22 09:56:44 -0500

    Add and test all collaborator model endpoints

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProjectViewSet
+from .views import UserViewSet, ProjectViewSet, CollaboratorViewSet
 
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'projects', ProjectViewSet)
-
+router.register(r'collaborators', CollaboratorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
